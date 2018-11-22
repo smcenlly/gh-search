d48ad1c830ae54db5cc59ef1aa07dccad13f1a7f
Simon McEnlly <simon.mcenlly@gmail.com>
2018-11-23 08:49:37 +1000

    Add ./config/ javascript files to Wallaby config

--- wallaby.js
+++ wallaby.js
@@ -1,6 +1,7 @@
 module.exports = function(wallaby) {
     return {
         files: [
+            { pattern: 'config/**/*.js', instrument: false },
             "src/**/*.+(js|jsx|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)",
             "!src/__mocks/**/*.js?(x)",
             "!src/**/*.test.js?(x)"
